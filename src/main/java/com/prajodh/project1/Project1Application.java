a2c2f7cd3ff78572676fd6b1c8588ab0b155c1f3
prajodh <prajodhav@gmail.com>
2021-01-19 15:31:33 -0600

    branch1 1st commit

--- src/main/java/com/prajodh/project1/Project1Application.java
+++ src/main/java/com/prajodh/project1/Project1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Project1Application {
 
 	public static void main(String[] args) {
+		System.out.println("Hi!!!!!!!!!!!!!!!!!!!");
 		SpringApplication.run(Project1Application.class, args);
 	}
-
 }
